81230e75686d135cff8ef36af49545c77bd1fef8
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-02 13:26:39 +0100

    V1.0

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000); 
+    }, 1000); 
 
     return () => clearTimeout(timer); 
   }, []);
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,28 +3,22 @@ import banner from "../Assets/Images/mybanner.png"
 import "../Assets/Styles/hero.css"
 const Hero = () => {
   return (
-    <div>
-        <div className="container" id="home">
-        <div className="hero_container">
-          <div className="container-fluid">
-            <div className="content">
-              <div className="row details">
-                <div className="col-lg-6 image">
-                <img src={banner} className='main' alt="" />
-                </div>
-                <div className="col-lg-6 text">
-                  <h3 className="stroke">Gabriel</h3>
-                  <h3>Bayode</h3>
-                  <div className="job">
-                  <span>FrontEnd Design</span><span>BackEnd Development</span><span>API</span>
-                  </div>
-                </div>
-              </div>
+   <div>
+    <div className="container hero_container">
+      <div className="row details">
+          <div className="col-lg-6 image">
+            <img src={banner} className='main' alt="" />
+          </div>
+          <div className="col-lg-6 text">
+            <h3 class="stroke">Gabriel</h3>
+            <h3>Bayode</h3>
+            <div class="job">
+            <span>FrontEnd Design</span><span>BackEnd Development</span><span>API</span>
             </div>
           </div>
-        </div>
-        </div>
+      </div>
     </div>
+   </div>
   )
 }
 
