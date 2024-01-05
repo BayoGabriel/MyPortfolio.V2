3f21fa758370d037468940463ee37f420e1d6a50
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-05 14:59:33 +0100

    V1.0

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import "../Assets/Styles/about.css"
 import Accordion from 'react-bootstrap/Accordion';
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
@@ -11,7 +12,17 @@ import rn from "../Assets/Images/react.512a3a7e.svg"
 import redux from"../Assets/Images/redux.2fdafc8e.svg"
 import express from "../Assets/Images/express-js.1f78831a.svg"
 import react from "../Assets/Images/react.svg"
+import Nodejs from "../Assets/Images/node.fc4d4b87.svg"
+import Flask from "../Assets/Images/icons8-flask.svg"
+import git from "../Assets/Images/git.21d80414.svg"
+import github from "../Assets/Images/github.svg"
+import ts from "../Assets/Images/typescript.97711300.svg"
+import Python from "../Assets/Images/icons8-python.svg"
+import NoSQL from "../Assets/Images/mongoDB.d8d2b67f.svg"
 import ProgressBar  from "react-bootstrap/ProgressBar";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function CustomToggle({ children, eventKey }) {
     const decoratedOnClick = useAccordionButton(eventKey, () =>
@@ -27,10 +38,13 @@ function CustomToggle({ children, eventKey }) {
     );
   }
 const About = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <div>
-            <div className="container" id="about">
-                <div className="row">
+            <div className="container" id="about" data-aos="fade-up">
+                <div className="row" data-aos-duration="3000">
                     <div className="col-lg-6 left">
                         <div class="about_title">
                         <span>Gabriel Bayode</span>
@@ -68,7 +82,7 @@ const About = () => {
                         </Card>
                         <Card className="each">
                             <Card.Header className="header">
-                            <CustomToggle eventKey="1" className="tit">FrontEnd Stack</CustomToggle>
+                            <CustomToggle eventKey="1" className="tit">Front-End Stack</CustomToggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="1">
                             <Card.Body>
@@ -107,7 +121,7 @@ const About = () => {
                         </Card>
                         <Card className="each">
                             <Card.Header className="header">
-                            <CustomToggle eventKey="2" className="tit">BackEnd Stack</CustomToggle>
+                            <CustomToggle eventKey="2" className="tit">Back-End Stack</CustomToggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="2">
                             <Card.Body>
@@ -117,29 +131,44 @@ const About = () => {
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
                                     </div>
                                     <div className="ass">
-                                        <img src={css} alt="svf" /> <span>Css</span>
+                                        <img src={Nodejs} alt="svf" /> <span>Node.js</span>
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
                                     </div>
                                     <div className="ass">
-                                        <img src={bootstrap} alt="svf" /> <span>Bootstrap</span>
+                                        <img src={Python} alt="svf" /> <span>Python</span>
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
                                     </div>
                                     <div className="ass">
-                                        <img src={javascript} alt="svf" /> <span>JavaScript</span>
+                                        <img src={Flask} alt="svf" /> <span>Flask</span>
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
                                     </div>
                                     <div className="ass">
-                                        <img src={react} alt="svf" /> <span>React</span>
+                                        <img src={NoSQL} alt="svf" /> <span>NoSQL (MongoDB)</span>
+                                        <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
+                                    </div>                                    
+                                </div>
+                            </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card className="each">
+                            <Card.Header className="header">
+                            <CustomToggle eventKey="3" className="tit">Others</CustomToggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="3">
+                            <Card.Body>
+                            <div className="first">
+                                    <div className="ass">
+                                        <img src={git} alt="svf" /> <span>Git</span>
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
                                     </div>
                                     <div className="ass">
-                                        <img src={rn} alt="svf" /> <span>React Native</span>
+                                        <img src={github} alt="svf" /> <span>GitHub</span>
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
-                                    </div>
+                                    </div>    
                                     <div className="ass">
-                                        <img src={redux} alt="svf" /> <span>Redux</span>
+                                        <img src={ts} alt="svf" /> <span>TypeScript</span>
                                         <ProgressBar now={95} style={{height: '5px', marginTop: '5px'}}/>
-                                    </div>
+                                    </div>                            
                                 </div>
                             </Card.Body>
                             </Accordion.Collapse>
