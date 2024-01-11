c1b9ddd965bf25a25a397ab64abf3c86f65cde3c
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-11 20:57:11 +0100

    V1.0

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Assets/Styles/footer.css'
+import logo from  '../Assets/Images/logo.jpg'
+const Footer = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Footer
