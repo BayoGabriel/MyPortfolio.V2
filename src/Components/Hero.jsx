044ea49b7f3b76cebba889cf4e5eade9bb04f74b
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-13 15:03:17 +0100

    V1.0

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,25 +12,21 @@ const Hero = () => {
 }, [])
   return (
    <div id='home'>
-    <div className='hero_container'>
-    <div className="container">
-      <div className='content'>
+    <div className='container-lg hero_container'>
       <div className="row details">
-          <div className="col-lg-6 col-md-6 col-sm-12 image" data-aos="fade-up" data-aos-duration="3000">
-            <img src={banner} className='main' alt="ghty" />
+        <div className="col-lg-6 col-md-6 col-sm-12 image" data-aos="fade-up" data-aos-duration="3000">
+          <img src={banner} className='main' alt="ghty" />
+        </div>
+        <div className="col-lg-6 md-6 col-sm-12" data-aos="fade-left" data-aos-duration="3000">
+          <div className='text'>
+          <h3 className="stroke">Gabriel</h3>
+          <h3>Bayode</h3>
+          <div class="job">           
+          <span>Front-End Design</span><span>Back-End Development</span>
           </div>
-          <div className="col-lg-6 md-6 col-sm-12" data-aos="fade-left" data-aos-duration="3000">
-            <div className='text'>
-            <h3 className="stroke">Gabriel</h3>
-            <h3>Bayode</h3>
-            <div class="job">           
-            <span>Front-End Design</span><span className="dot">+</span><span>Back-End Development</span>
-            </div>
-            </div>
           </div>
+        </div>
       </div>
-      </div>
-    </div>
     </div>
    </div>
   )
