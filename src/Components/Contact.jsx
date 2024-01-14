9e15d6dbae1af516dd045cf35c5e4fe1d1030cd5
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-14 19:17:18 +0100

    V1.0

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,85 +1,39 @@
 import React from 'react'
-import "../Assets/Styles/contact.css"
-import "../Assets/Styles/about.css"
-import Accordion from 'react-bootstrap/Accordion';
-import { useAccordionButton } from 'react-bootstrap/AccordionButton';
-import Card from 'react-bootstrap/Card';
-
-function CustomToggle({ children, eventKey }) {
-  const decoratedOnClick = useAccordionButton(eventKey, () =>
-    console.log('totally custom!'),
-  );
-
-  return (
-    <div
-      onClick={decoratedOnClick}
-    >
-      {children}
-    </div>
-  );
-}
+import mal from "../Assets/Images/mail.svg"
+import x from "../Assets/Images/twitter2.svg"
+import ln from "../Assets/Images/linkedin2.svg"
+import gth from "../Assets/Images/github.svg"
 
 const Contact = () => {
   return (
-    <div id='contact'>
-        <div className="container-lg contact_section">
-            <div className="header">
-                <span>Portfolio</span>
-                <h3>Get In Touch</h3>
+    <div>
+      <div className="container-lg">
+        <div className="row">
+          <div className="col-lg-6">
+            <h3>Let's Talk</h3>
+            <p>Have some big idea or brand to develop and need help? Then reach out we'd love to hear about your project and provide help.</p>
+            <h4>Email</h4>
+            <div className="mage">
+             <img src={mal} alt="mal" />
+             <a href="mailto:bayogabriel24@gmail.com">Bayogabriel24@gmail.com</a>
             </div>
-            <Accordion defaultActiveKey="0">
-                            <Card className="each">
-                            <Card.Header className="heade">
-                            <CustomToggle eventKey="0" className="tit">WORKING DAYS</CustomToggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                            <Card.Body>
-                              <p>
-                              Our company provides services for our customers between 9:00 a.m. and 8:00 p.m. ET, Monday through Friday. You can visit or call during these days.
-                              <br />
-                              Weekends: Saturday and Sunday.
-                              </p>
-                            </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card className="each">
-                            <Card.Header className="heade">
-                            <CustomToggle eventKey="1" className="tit">SOCIAL PROFILES</CustomToggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="1">
-                            <Card.Body>                            
-                            </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card className="each">
-                            <Card.Header className="heade">
-                            <CustomToggle eventKey="2" className="tit">EXTRA INFO</CustomToggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="2">
-                            <Card.Body>
-                            </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card className="each">
-                            <Card.Header className="heade">
-                            <CustomToggle eventKey="3" className="tit">FILL A FORM</CustomToggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="3">
-                            <Card.Body>
-                            </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card className="each">
-                            <Card.Header className="heade">
-                            <CustomToggle eventKey="4" className="tit">LOCATION</CustomToggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="4">
-                            <Card.Body>
-                            </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        </Accordion>
+            <h4>Socials</h4>
+            <div className="soc">
+            <img src={gth} alt="mal" />
+            <img src={ln} alt="mal" />
+            <img src={x} alt="mal" />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <form action="">
+            <input type="text" placeholder='Name' name='name'/>
+            <input type="email" placeholder='Email' name='email' />
+            <input type="text" placeholder='SUbject' name='subject' />
+            <input type="text" placeholder='Your message' name='message' />
+            </form>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
