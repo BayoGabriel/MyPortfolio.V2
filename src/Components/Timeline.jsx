cd07b8a5c04a0fb6e0916d2b04d175bbeda6c9bc
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-15 21:02:06 +0100

    V1.0

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -4,10 +4,58 @@ import "../Assets/Styles/timeline.css"
 const Timeline = () => {
   return (
     <div id='timeline'>
-        <div className="container timeline_section">
+        <div className="container-lg timeline_section erling_tm_experience">
             <div className="header">
                 <span>Timeline</span>
                 <h3>My Timeline</h3>
+            </div>
+            <div className="row">
+              <div className="col-md-6 col-sm-12 content">
+              <div class="left">
+<div class="box">
+<div class="title wow fadeInLeft" data-wow-duration="1s">
+<h3>Experience</h3>
+</div>
+<div class="list_wrap">
+<ul>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Envato Market</h3>
+<span>Web Designer</span>
+</div>
+<div class="time">
+<span>2020-now</span>
+</div>
+</div>
+</li>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Behance</h3>
+<span>SEO Optimizer</span>
+</div>
+<div class="time">
+<span>2018-2020</span>
+</div>
+</div>
+</li>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Colorlib</h3>
+<span>Theme Reviewer</span>
+</div>
+<div class="time">
+<span>2016-2018</span>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+              </div>
             </div>
         </div>
     </div>
---
+++ src/Components/test.jsx
@@ -0,0 +1,102 @@
+// tmlne
+
+<div class="erling_tm_section clearfix" id="timeline">
+<div class="erling_tm_experience">
+<div class="container">
+<div class="erling_tm_title wow fadeInLeft" data-wow-duration="1s">
+<span>Timeline</span>
+<h3>Timeline Period</h3>
+</div>
+<div class="experience_inner">
+{/* <div class="left">
+<div class="box">
+<div class="title wow fadeInLeft" data-wow-duration="1s">
+<h3>Experience</h3>
+</div>
+<div class="list_wrap">
+<ul>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Envato Market</h3>
+<span>Web Designer</span>
+</div>
+<div class="time">
+<span>2020-now</span>
+</div>
+</div>
+</li>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Behance</h3>
+<span>SEO Optimizer</span>
+</div>
+<div class="time">
+<span>2018-2020</span>
+</div>
+</div>
+</li>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Colorlib</h3>
+<span>Theme Reviewer</span>
+</div>
+<div class="time">
+<span>2016-2018</span>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div> */}
+<div class="right">
+<div class="box">
+<div class="title wow fadeInLeft" data-wow-duration="1s">
+<h3>Education</h3>
+</div>
+<div class="list_wrap">
+<ul>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Univercity of Texas</h3>
+<span>Master of Design</span>
+</div>
+<div class="time">
+<span>2020-2017</span>
+</div>
+</div>
+</li>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Webster College</h3>
+<span>UI/UX Design</span>
+</div>
+<div class="time">
+<span>2017-2015</span>
+</div>
+</div>
+</li>
+<li class="wow fadeInLeft" data-wow-duration="1s">
+<div class="list_inner">
+<div class="occ">
+<h3>Github Club</h3>
+<span>Web Sertification</span>
+</div>
+<div class="time">
+<span>2013-2015</span>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
