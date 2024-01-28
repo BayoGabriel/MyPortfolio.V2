ef1d0f13fbaf0f6f7fd41ea94b2c6c420a35e460
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-28 16:46:12 +0100

    V1.0

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -30,7 +30,7 @@ const Portfolio = () => {
               <h3>Recent Works</h3>
             </div>
             <div className="row">
-              <div className="col-xs-6"></div>
+              <div className="col--6"></div>
             </div>
          </div>
         </div>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -12,8 +12,9 @@ const Services = () => {
         AOS.init();
     }, [])
   return (
-    <div id='service'> 
-        <div className="container-lg service_section">
+    <> 
+        <div className="container-lg">
+            <div className="servcunt" id='service'>
             <div className="header" data-aos="fade-up" data-aos-duration="2000">
                 <span>Services</span>
                 <h3>Quality Services</h3>
@@ -56,8 +57,9 @@ const Services = () => {
                     </div>
                 </div>
             </div>
+            </div>
         </div>
-    </div>
+    </>
   )
 }
 
