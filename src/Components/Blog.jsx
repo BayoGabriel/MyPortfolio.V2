451915ed67cc7f492f3c0f1a7aac3383068d4841
Bayode Gabriel <Bayogabriel24@gmail.com>
2024-01-29 22:56:37 +0100

    V1.0

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import  "../Assets/Styles/blog.css"
-import blog1 from "../Assets/Images/kk.jpg"
-import blog2 from "../Assets/Images/kk.jpg"
-import blog3 from "../Assets/Images/kk.jpg"
+import blog1 from "../Assets/Images/portfolio-image-3.jpg"
+import blog2 from "../Assets/Images/portfolio-image-2.jpg"
+import blog3 from "../Assets/Images/portfolio-image-6.jpg"
 
 const Blog = () => {
   return (
@@ -15,21 +15,61 @@ const Blog = () => {
             </div>
             <div className="row dfghj">
                   <div className="col-lg-4 col-md-6 col-sm-12 newsrow">
+                    <div className="image">
                     <img src={blog1} alt="" />
+                    </div>
                     <div className="details">
                       <div className="meta">
                         <span className="admin">
-                          By
-                          <a href="#blog" className="line-effect">Bayode</a>
+                          <a href="#blog" className="line_effect">Bayode</a>
                         </span>
-                        <span className="category">
-                          In 
-                          <a href="#blog" className="line-effect">Web Design</a>
+                        <span className="admin">
+                          <a href="#blog" className="line_effect">Web Design</a>
                         </span>
                       </div>
                       <div className="title">
                         <h3>
                           <a href="#blog" className="text_hover_effect">Secrets of the Mobile Application</a>
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 col-sm-12 newsrow">
+                    <div className="image">
+                    <img src={blog2} alt="" />
+                    </div>
+                    <div className="details">
+                      <div className="meta">
+                        <span className="admin">
+                          <a href="#blog" className="line_effect">Bayode</a>
+                        </span>
+                        <span className="admin">
+                          <a href="#blog" className="line_effect">Web Design</a>
+                        </span>
+                      </div>
+                      <div className="title">
+                        <h3>
+                          <a href="#blog" className="text_hover_effect">The art of programming</a>
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 col-sm-12 newsrow">
+                    <div className="image">
+                    <img src={blog3} alt="" />
+                    </div>
+                    <div className="details">
+                      <div className="meta">
+                        <span className="admin">
+                          <a href="#blog" className="line_effect">Bayode</a>
+                        </span>
+                        <span className="admin">
+                          <a href="#blog" className="line_effect">Web Design</a>
+                        </span>
+                      </div>
+                      <div className="title">
+                        <h3>
+                          <a href="#blog" className="text_hover_effect">Tips on brand design and positioning</a>
                         </h3>
                       </div>
                     </div>
